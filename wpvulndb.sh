5c8df012b10d856de713a3f360680ccb1451d3f8
Vinoth kumar <vinothpkumar333@gmail.com>
2020-02-24 18:24:22 +0530

    Update wpvulndb.sh

--- wpvulndb.sh
+++ wpvulndb.sh
@@ -1,6 +1,6 @@
 #Simple script to look for Vulnerabilities in Wordpress plugins
 
 for plugin in `cat plugins_list`;do #List of plugins used in your Wordpress
-    echo "Plugin Name:" $plugin
+  echo "Plugin Name:" $plugin
   curl -H "Authorization: Token token=wpvulndb_token_goes_here" https://wpvulndb.com/api/v3/plugins/$plugin #Refer https://wpvulndb.com/api
 done
